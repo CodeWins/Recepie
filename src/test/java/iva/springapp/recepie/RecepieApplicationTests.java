a112bc1a59d59ec4a85ba54d34e0a669730c7283
Igor Varshavsky <igor.varshavsky@gmail.com>
2021-02-19 00:08:14 -0500

    Initial Project Structure

---
+++ src/test/java/iva/springapp/recepie/RecepieApplicationTests.java
@@ -0,0 +1,13 @@
+package iva.springapp.recepie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecepieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
